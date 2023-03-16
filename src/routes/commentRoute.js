cd6ca6fbe9aca54af9c84bc8309b342fd26a2953
viewee <marnich@naver.com>
2023-03-17 05:23:33 +0900

    섹션10완

--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -4,57 +4,71 @@ const { Blog, User, Comment } = require("../models");
 const { isValidObjectId, startSession } = require("mongoose");
 
 commentRouter.post("/", async (req, res) => {
-  const session = await startSession();
+  // const session = await startSession();
   let comment;
   try {
-    await session.withTransaction(async () => {
-      const { blogId } = req.params;
-      const { content, userId } = req.body;
-      if (!isValidObjectId(blogId))
-        return res.status(400).send({ error: "blogId is invalid" });
-      if (!isValidObjectId(userId))
-        return res.status(400).send({ error: "userId is invalid" });
-      if (typeof content !== "string")
-        return res.status(400).send({ error: "content is required" });
+    // await session.withTransaction(async () => {
+    const { blogId } = req.params;
+    const { content, userId } = req.body;
+    if (!isValidObjectId(blogId))
+      return res.status(400).send({ error: "blogId is invalid" });
+    if (!isValidObjectId(userId))
+      return res.status(400).send({ error: "userId is invalid" });
+    if (typeof content !== "string")
+      return res.status(400).send({ error: "content is required" });
 
-      const [blog, user] = await Promise.all([
-        Blog.findById(blogId, {}, { session }),
-        User.findById(userId, {}, { session }),
-      ]);
+    const [blog, user] = await Promise.all([
+      Blog.findById(blogId, {}, {}),
+      User.findById(userId, {}, {}),
+    ]);
 
-      if (!blog || !user)
-        return res.status(400).send({ error: "blog or user not exist" });
-      if (!blog.islive)
-        return res.status(400).send({ error: "blog is not available" });
+    if (!blog || !user)
+      return res.status(400).send({ error: "blog or user not exist" });
+    if (!blog.islive)
+      return res.status(400).send({ error: "blog is not available" });
 
-      comment = new Comment({
-        content,
-        user,
-        userFullName: `${user.name.first} ${user.name.last}`,
-        blog: blogId,
-      });
-      // await Promise.all([
-      //   comment.save(),
-      //   Blog.updateOne({ _id: blogId }, { $push: { comments: comment } }),
-      // ]);
-      await session.abortTransaction();
-
-      blog.commentsCount++;
-      blog.comments.push(comment);
-      // 첫번째 객체가 날라감
-      if (blog.commentsCount > 3) blog.comments.shift();
-
-      await Promise.all([
-        comment.save({ session }),
-        blog.save({}),
-        // Blog.updateOne({ _id: blogId }, { $inc: { commentsCount: 1 } }),
-      ]);
+    comment = new Comment({
+      content,
+      user,
+      userFullName: `${user.name.first} ${user.name.last}`,
+      blog: blogId,
     });
+    // await Promise.all([
+    //   comment.save(),
+    //   Blog.updateOne({ _id: blogId }, { $push: { comments: comment } }),
+    // ]);
+
+    // 세션이 중단되고, 이전까지 실행되었던 세션작업들 다 롤백하는 기능
+    // await session.abortTransaction();
+
+    // blog.commentsCount++;
+    // blog.comments.push(comment);
+    // // 첫번째 객체가 날라감
+    // if (blog.commentsCount > 3) blog.comments.shift();
+
+    // await Promise.all([
+    //   comment.save({}),
+    //   blog.save({}),
+    //   // Blog.updateOne({ _id: blogId }, { $inc: { commentsCount: 1 } }),
+    // ]);
+    // });
+
+    await Promise.all([
+      comment.save(),
+      Blog.updateOne(
+        { _id: blogId },
+        {
+          $inc: { commentsCount: 1 },
+          $push: { comments: { $each: [comment], $slice: -3 } },
+        }
+      ),
+    ]);
+
     return res.send({ comment });
   } catch (error) {
     return res.status(400).send({ error: error.message });
   } finally {
-    await session.endSession();
+    // await session.endSession();
   }
 });
 
